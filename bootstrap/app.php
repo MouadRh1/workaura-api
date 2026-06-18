f852349b730ea9a290a26b68f0f93aa32fdb9244
Mouad Rahmouni <mouadrah01@gmail.com>
2026-06-18 18:04:00 +0100

    configuration de repense par mail

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'admin' => AdminMiddleware::class,
+            'auth' => \App\Http\Middleware\Authenticate::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
